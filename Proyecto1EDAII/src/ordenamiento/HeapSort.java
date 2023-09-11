cdd9cb49daee11d7b374b4ec723fa458a658eae5
lued2897 <lued2897@gmail.com>
2023-09-10 22:31:53 -0600

    feat: added class  HeapSort

---
+++ Proyecto1EDAII/src/ordenamiento/HeapSort.java
@@ -0,0 +1,53 @@
+package ordenamiento;
+
+public class HeapSort {
+    private static int heapSize;
+    private static int count; 
+    
+    private static void heapify(int arr[],int i){
+        int l = (2*i) + 1;
+        int r = (2*i) + 2;
+        int largest;
+        
+        if(l <= heapSize && arr[l] > arr[i]){
+            largest = l;
+            count+=3;
+        }else{
+            largest = i;
+            count+=3;
+        }
+        if(r <= heapSize && arr[r] > arr[largest]){
+            largest =  r;
+            count +=3;
+        }
+        if(largest != i){
+            Utilerias.swap(arr, i, largest);
+            count+=2;
+            //print
+            heapify(arr,largest);
+        }
+    }
+    
+    private static void buildHeap(int arr[]){
+        heapSize = arr.length - 1;
+        for(int i = (arr.length-1)/2; i >= 0; i--){
+            heapify(arr, i);
+            count++; //contar comparaciones del for?
+        }
+    }
+    
+    public static int heapSort(int arr[]){
+        int size = arr.length; 
+        count=0;
+        buildHeap(arr);
+        for(int i = size - 1; i > 0 ; i-- ){
+            Utilerias.swap(arr,0 ,heapSize);
+            count+=2;
+            heapSize--;
+            //print
+            heapify(arr,0);
+        }
+        return count;
+    }
+    
+}
--- Proyecto1EDAII/src/ordenamiento/Principal.java
+++ Proyecto1EDAII/src/ordenamiento/Principal.java
@@ -8,12 +8,13 @@ public class Principal {
     
     public static void main(String[] args) {
         
-        int count,array[] = Utilerias.randArray(262144,1,1000);
+        int count,array[] = Utilerias.randArray(40,1,1000);
         System.out.println("Arreglo original: ");
         Utilerias.printArray(array);
         
-        System.out.println("\nEjecutando MergeSort");
-        count=MergeSort.mergeSort(array);
+        System.out.println("\nEjecutando HeapSort");
+        //count=MergeSort.mergeSort(array);
+        count = HeapSort.heapSort(array);
         System.out.println("Operaciones ejecutadas: "+count+"\n");
         
         System.out.println("Arreglo ordenado: ");
