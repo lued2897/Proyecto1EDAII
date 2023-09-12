98bb8d7c137b885538a9dfa51f8c95903b7bc304
Karlahs03 <143905490+Karlahs03@users.noreply.github.com>
2023-09-12 12:28:43 -0600

    BubbleSort.java

--- Proyecto1EDAII/src/ordenamiento/BubbleSort.java
+++ Proyecto1EDAII/src/ordenamiento/BubbleSort.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author karla
- */
 package ordenamiento; 
 public class BubbleSort {
     private static int count;
@@ -16,19 +8,16 @@ public class BubbleSort {
     for (i = n - 1; i > 0; i--) {
         boolean verificar = false; 
         for (j = 0; j < i; j++) {
-            count++;
+            count++; //se compara  a[j] y a[j + 1]
             if ( a[j] > a[j + 1]) { 
                 swap(a, j, j + 1); 
                 verificar = true;
-                count++;
+                count++; //Intercambiamos a[j] y a[j + 1]
             }
-            count++;
         }
-        count++;
        if (!verificar) 
            return count;
         }
-    count++;
     return count;
     
     }
