dc8f423f5bb6bb5167a475e571d83c31ec57ffa5
sammojica <112099657+sammojica@users.noreply.github.com>
2023-09-16 00:03:30 -0600

    Update Quick.java

--- Proyecto1EDAII/src/ordenamiento/Quick.java
+++ Proyecto1EDAII/src/ordenamiento/Quick.java
@@ -1,11 +1,12 @@
 package ordenamiento;
 public class Quick {
-    private static int count;
+    public static int count;
     public static int quickSort(int array[], int low, int high){
+        count = 0;
         if(low < high) {
             int piv = partition(array, low, high);
-            quickSort(array, low, (piv-1));
-            quickSort(array, (piv+1), high);
+            count += quickSort(array, low, (piv-1));
+            count +=quickSort(array, (piv+1), high);
             count +=2;
         }
         return count;
@@ -18,7 +19,7 @@ public class Quick {
             if (array[j] <= pivot) {
                 i++;
                 Utilerias.swap(array, i, j);
-                count++;
+                count+=2;
             }
         }
         Utilerias.swap(array, (i+1), high);
