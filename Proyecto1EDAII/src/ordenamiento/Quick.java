de38853416b0d78b959cb689e99446d6ccd04789
sammojica <112099657+sammojica@users.noreply.github.com>
2023-09-17 16:49:57 -0600

    javadoc

--- Proyecto1EDAII/src/ordenamiento/Quick.java
+++ Proyecto1EDAII/src/ordenamiento/Quick.java
@@ -1,11 +1,26 @@
 /**
  *Autor: Mojica Pereda Elena Samantha
- *Fecha:17 de septiembre
- *Clase para ordenar el arreglo a través de QuickSort
+ *Fecha última modificación:17 de septiembre
+ * La clase Quick proporciona una implementación del algoritmo de ordenamiento QuickSort.
+ * Este algoritmo divide una lista en sublistas más pequeñas, ordena cada lista y luego
+ * las combina para obtener una matriz ordenada.
  */
 package ordenamiento;
 public class Quick {
+     
+    /**
+     * Un contador que rastrea la cantidad total de operaciones realizadas durante el proceso de ordenamiento.
+     */
     public static int count;
+
+    /**
+     * Ordena una matriz utilizando el algoritmo QuickSort.
+     *
+     * @param array La matriz que se va a ordenar.
+     * @param low   El índice más bajo de la lista (generalmente 0).
+     * @param high  El índice más alto de la lista (generalmente la longitud de la matriz - 1).
+     * @return El número total de operaciones realizadas durante el proceso de ordenamiento.
+     */
     public static int quickSort(int array[], int low, int high){
         count = 0;
         if(low < high) {
@@ -16,6 +31,16 @@ public class Quick {
         }
         return count;
     }
+    
+    /**
+     * Particiona una lista en dos sublistas y coloca elementos menores o iguales al pivote
+     * en la sublista izquierda y elementos mayores en la sublista derecha.
+     *
+     * @param array La lista que se va a particionar.
+     * @param low   El índice más bajo de la lista.
+     * @param high  El índice más alto de la lista.
+     * @return El índice del pivote después de la partición.
+     */
 
     public static int partition (int array[], int low, int high) {
         int pivot = array[high];
